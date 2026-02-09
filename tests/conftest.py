43eb5fc3df1e097db1d752919b9cb3d6c86513ab
michal <hovin112@gmail.com>
2026-02-09 20:09:56 +0100

    airflow + docker base test implementation

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import subprocess
+
+
+@pytest.fixture(scope="session", autouse=True)
+def airflow_env():
+    subprocess.run(
+        ["docker", "compose", "-f", "docker-compose.test.yml", "up", "-d"],
+        check=True,
+    )
+
+    yield
+
+    subprocess.run(
+        ["docker", "compose", "-f", "docker-compose.test.yml", "down", "-v"],
+        check=True
+    )
