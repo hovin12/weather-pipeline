e1478839da1f00ec8a4d45479a2d87a5ba3316a8
michal <hovin112@gmail.com>
2026-02-10 18:37:15 +0100

    removed redundant logs in iter_table

--- src/db/city_params.py
+++ src/db/city_params.py
@@ -1,12 +1,7 @@
 from sqlalchemy import text
-import logging
-
-
-logger = logging.getLogger(__name__)
 
 
 def iter_table(engine, table):
-    logger.info(f"iterating table {table}")
 
     with engine.connect().execution_options(stream_results=True) as conn:
         result = conn.execute(text(f"SELECT * FROM {table}"))
