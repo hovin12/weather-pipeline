f0939a656d11081a214b1223f0e03f8ba08ec4b5
michal <hovin112@gmail.com>
2026-02-17 21:17:06 +0100

    added stations to init.sql

--- tests/sql/init.sql
+++ tests/sql/init.sql
@@ -42,3 +42,50 @@ CREATE TABLE cities (
 COPY cities
 FROM '/data/cities.csv'
 WITH (FORMAT csv, HEADER true);
+
+CREATE TABLE stations (
+    city VARCHAR,
+    direction VARCHAR(1),
+    lat DECIMAL,
+    lon DECIMAL
+);
+
+COPY stations
+FROM '/data/stations.csv'
+WITH (FORMAT csv, HEADER true);
+
+CREATE TABLE IF NOT EXISTS raw_stations (
+    run_ts TIMESTAMP,
+    city VARCHAR,
+    direction VARCHAR(1),
+    payload JSONB
+);
+
+ALTER TABLE raw_stations ADD PRIMARY KEY (run_ts, city, direction);
+
+CREATE TABLE IF NOT EXISTS weather_stations (
+    run_ts TIMESTAMP,
+    city VARCHAR,
+    direction VARCHAR(1),
+    timestamp TIMESTAMP,
+    sunset TIMESTAMP,
+    sunrise TIMESTAMP,
+    temp DECIMAL,
+    temp_max DECIMAL,
+    temp_min DECIMAL,
+    perceived_temp DECIMAL,
+    humidity INT,
+    pressure INT,
+    pressure_ground INT,
+    wind_deg INT,
+    wind_speed DECIMAL,
+    wind_gust DECIMAL,
+    clouds INT,
+    rain DECIMAL,
+    snow DECIMAL,
+    label VARCHAR,
+    description VARCHAR,
+    visibility INT
+);
+
+ALTER TABLE weather_stations ADD PRIMARY KEY (run_ts, city, direction);
